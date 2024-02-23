328a6cc629ba079ce2fff07ce0477fb7e4f5f016
Rhustish <bbhatnagar27@gmail.com>
2024-02-23 18:24:17 +0530

    added fucntions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { readPdfText } from "pdf-text-reader";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
+
+  const reader = async (e)=>{
+    alert(e)
+    
+    // const text = await readPdfText({data:e.file})
+    // console.log(text);
+    
+  }
+
   const [file, setfile] = useState('');
   const handleFileRead = (e)=>{
     setfile(e);
+  }
+  const fire = ()=>{
+    if(file === ''){
+      alert("Upload a file please")
+    }
+    else{reader(file);}
   }
 
   return (
@@ -14,9 +30,12 @@ function App() {
         Hi, Get Ready Fot Your Upcoming Exams
         <br /> Give Us Your Material and Test Your Knowledge <br />
         <input type="file" id="box"onChange={e=>handleFileRead(e.target.files[0])}/>
-
-        
+        <div className="button" onClick={()=>fire()}>
+        GO!
       </div>
+      </div>
+
+      
     </div>
   );
 }
